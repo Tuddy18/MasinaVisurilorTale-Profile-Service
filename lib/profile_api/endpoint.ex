32b44ed62c4e80877608aac7e1fc1fb2cfe1aa61
tuddy <tuddy18_26@yahoo.com>
2020-05-25 23:40:27 +0300

    set cors plug in endpoint

--- lib/profile_api/endpoint.ex
+++ lib/profile_api/endpoint.ex
@@ -14,6 +14,7 @@ defmodule Profiles.Endpoint do
     json_decoder: Poison
   )
 #  plug Profiles.AuthPlug
+    plug CORSPlug, origin: "*"
   plug(:dispatch)
 
   post "/login", private: @skip_token_verification do
